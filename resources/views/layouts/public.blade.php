e89a0cf1346432a011e8e21d558cf848804b85a6
Emanuele <73951371+EmanueleMacera@users.noreply.github.com>
2026-01-14 16:48:16 +0100

    UPDATE: add contact-page.css to layout head

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -16,6 +16,7 @@
 <link rel="manifest" href="/site.webmanifest" />
 <link rel="stylesheet" href="{{ asset('css/styles.css') }}?v={{ config('app.version') }}">
 <link rel="stylesheet" href="{{ asset('css/about-page.css') }}?v={{ config('app.version') }}">
+<link rel="stylesheet" href="{{ asset('css/contact-page.css') }}?v={{ config('app.version') }}">
 </head>
 <body>
 <header class="topbar">
