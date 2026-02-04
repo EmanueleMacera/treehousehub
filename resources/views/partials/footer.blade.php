a94e9a49492d85a7cd34fde63788bf1f01ccfc0c
Emanuele <73951371+EmanueleMacera@users.noreply.github.com>
2026-02-04 15:34:12 +0100

    Style shared footer using existing theme classes (no Bootstrap classes)

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,22 +2,20 @@
     $locale = app()->getLocale();
 @endphp
 
-<footer class="border-top mt-5 py-4 bg-body-tertiary">
+<footer class="footer">
     <div class="container">
-        <div class="row g-4">
-            <div class="col-12 col-md-6">
-                <h5 class="mb-3">{{ __('footer.contact_title') }}</h5>
+        <div class="footer__grid">
+            <div>
+                <div class="footer__title">{{ __('footer.contact_title') }}</div>
 
-                <div class="small text-body-secondary">
-                    <div class="mb-1">
-                        {{ __('footer.email_label') }}
-                        <a href="mailto:info@treehouseitalia.it">info@treehouseitalia.it</a>
+                <div class="footer__contact">
+                    <div>
+                        {{ __('footer.email_label') }} <a href="mailto:info@treehouseitalia.it">info@treehouseitalia.it</a>
                     </div>
-                    <div class="mb-1">
-                        {{ __('footer.phone_label') }}
-                        <a href="tel:+390198387211">+39 019 8387211</a>
+                    <div>
+                        {{ __('footer.phone_label') }} <a href="tel:+390198387211">+39 019 8387211</a>
                     </div>
-                    <div class="mb-1">
+                    <div>
                         {{ __('footer.vat_label') }} IT01581160098
                     </div>
                     <div>
@@ -26,9 +24,9 @@
                 </div>
             </div>
 
-            <div class="col-12 col-md-6">
-                <h5 class="mb-3">{{ __('footer.useful_links') }}</h5>
-                <ul class="list-unstyled small mb-0">
+            <div>
+                <div class="footer__title">{{ __('footer.useful_links') }}</div>
+                <ul class="footer__links">
                     <li><a href="{{ route('legal.privacy', ['locale' => $locale]) }}">{{ __('legal.privacy.title') }}</a></li>
                     <li><a href="{{ route('legal.cookies', ['locale' => $locale]) }}">{{ __('legal.cookies.title') }}</a></li>
                     <li><a href="{{ route('legal.terms', ['locale' => $locale]) }}">{{ __('legal.terms.title') }}</a></li>
@@ -39,13 +37,13 @@
             </div>
         </div>
 
-        <div class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center mt-4 pt-3 border-top small text-body-secondary">
-            <div>© {{ date('Y') }} {{ config('app.name', 'TreehouseHub') }}.</div>
-            <div class="mt-2 mt-md-0">
-                <a class="text-body-secondary" href="{{ route('legal.privacy', ['locale' => $locale]) }}">{{ __('legal.privacy.title') }}</a>
-                <span class="mx-2">•</span>
-                <a class="text-body-secondary" href="{{ route('legal.cookies', ['locale' => $locale]) }}">{{ __('legal.cookies.title') }}</a>
-            </div>
+        <div class="footer__bottom">
+            <small>© {{ date('Y') }} {{ config('app.name', 'TreeHouse Italia') }}</small>
+            <small>
+                <a href="{{ route('legal.privacy', ['locale' => $locale]) }}">{{ __('legal.privacy.title') }}</a>
+                <span aria-hidden="true"> • </span>
+                <a href="{{ route('legal.cookies', ['locale' => $locale]) }}">{{ __('legal.cookies.title') }}</a>
+            </small>
         </div>
     </div>
 </footer>
